5b9c27e9ceeab4b17547aebd2580d4b2979cce96
ibtesamahmed2004 <ibtesama964@gmail.com>
2025-07-26 00:06:06 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+const { model } = mongoose;
+
+const UserSchema = new Schema({
+    email: { type: String, required: true },
+    name: { type: String},
+    username: { type: String, required: true },
+    profilepic: {type: String},
+    coverpic: {type: String},
+    razorpayid: { type: String },
+    razorpaysecret: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+})
+
+
+export default mongoose.models.User || model("User", UserSchema);;
